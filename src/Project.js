b81849b85f310cec94976313b6c517fe4b51942a
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2017-12-12 11:46:47 +0100

    Async image detection

--- src/Project.js
+++ src/Project.js
@@ -23,6 +23,37 @@ class Project extends Component {
     };
   }
 
+  componentDidMount() {
+    var xhr = new XMLHttpRequest();
+    var base = 'data/img/';
+    var name = this.state.image;
+    var type = ['.gif'];
+    var element = this;
+
+    var error = function(e) {
+      if(type[0] === '.gif') {
+        type[0] = '.jpg';
+      } else if(type[0] === '.jpg') {
+        type[0] = '.png';
+      } else return;
+      xhr.open("GET",base+name+type[0],true);
+      xhr.send(null);
+    }
+    var found = function(e) {
+      if(xhr.responseText.startsWith('<!doctype html>')) {
+        error(null);
+      } else {
+        element.setState({imageUrl:base+name+type[0]});
+        xhr.abort();
+      }
+    }
+
+    xhr.open("GET",base+name+type,true);
+    xhr.onload = found;
+    xhr.onerror = error;
+    xhr.send(null);
+  }
+
   toggle() {
     for (let item of this.state.row)
       item.setState(prev => {
@@ -31,25 +62,9 @@ class Project extends Component {
       });
   }
 
-  urlExists(url){
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    return http.status !== 404;
-  }
-
   style() {
-    var base = "data/img/";
-    var name = this.state.image;
-    var type = ".";
-    if(this.urlExists(base+name+".gif"))
-      type = ".gif";
-    else if(this.urlExists(base+name+".jpg"))
-      type = ".jpg";
-    else if(this.urlExists(base+name+".png"))
-      type = ".png";
     return {
-      "backgroundImage":"linear-gradient(rgba(0, 0, 0, 0.55),rgba(0, 0, 0, 0.55)),url("+base+name+type+")"
+      "backgroundImage":"linear-gradient(rgba(0, 0, 0, 0.55),rgba(0, 0, 0, 0.55)),url("+this.state.imageUrl+")"
     };
   }
 
